6c713f72f8bbfb3e607980bcb271fb7556ffb488
abe01 <digital24s@gmail.com>
2025-10-31 12:08:51 +0000

    初めてのPHPファイルを作成し、基本的なHTML構造を出力するコードを追加

---
+++ hello.php
@@ -0,0 +1,19 @@
+
+<?php
+// PHPコードの開始タグ。ここからサーバーでPHPが処理されます。
+
+    // PHPの機能を使い、HTMLタグを含む文字列を出力します。
+    // echoは、指定された文字列をそのままウェブブラウザに出力する（送り返す）命令です。
+    echo "<!DOCTYPE html>";
+    echo "<html lang=\"ja\">";
+    echo "<head>";
+    echo "    <meta charset=\"UTF-8\">";
+    echo "    <title>PHP初体験</title>";
+    echo "</head>";
+    echo "<body>";
+    echo "<h1>";
+    echo "Hello, World!";
+    echo "</h1>";
+    echo "    <p>初めてのPHPファイルが実行されました。</p>";
+?>
+
